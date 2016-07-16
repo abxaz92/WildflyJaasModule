e70d200359e9ba2b7683235d7887795a85a9f057
david <chaava01@gmail.com>
2016-07-16 19:12:18 +0300

    replica set

--- src/main/java/ru/macrobit/security/ConnectionProvider.java
+++ src/main/java/ru/macrobit/security/ConnectionProvider.java
@@ -1,34 +1,35 @@
 package ru.macrobit.security;
 
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConnectionProvider {
-	private static MongoClient instance;
+    private static MongoClient instance;
 
-	private ConnectionProvider() {
-	}
+    private ConnectionProvider() {
+    }
 
-	public static MongoClient getConnection(String host, String user,
-			String pwd, String dbname) {
-		if (instance != null)
-			return instance;
-		List<ServerAddress> seed = new ArrayList<ServerAddress>();
-		try {
-			seed.add(new ServerAddress(host));
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-		List<MongoCredential> credentials = new ArrayList<MongoCredential>();
-		credentials.add(MongoCredential.createCredential(user, dbname,
-				pwd.toCharArray()));
-		instance = new MongoClient(seed, credentials);
-		return instance;
-	}
+    public static MongoClient getConnection(String host, String user,
+                                            String pwd, String dbname) {
+        if (instance != null)
+            return instance;
+        List<ServerAddress> seed = new ArrayList<>();
+        try {
+            seed.add(new ServerAddress(host));
+            seed.add(new ServerAddress("c2"));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+        credentials.add(MongoCredential.createCredential(user, dbname,
+                pwd.toCharArray()));
+        instance = new MongoClient(seed, credentials);
+        return instance;
+    }
 
 }
